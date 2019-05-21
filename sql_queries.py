3871accc4148c767b1a0023cdf6be758e936e86d
Justin Bricker <jtbricker@infiniteenergy.com>
2019-05-20 22:52:29 -0400

    Add insert statements

--- sql_queries.py
+++ sql_queries.py
@@ -135,12 +135,24 @@ songplay_table_insert = ("""
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (
+        SELECT userId, firstName, lastName, gender, level
+        FROM staging_events
+    )
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (
+        SELECT song_id, title, artist_id, year, duration
+        FROM staging_songs
+    )
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (
+	    SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude 
+        FROM staging_songs
+    )
 """)
 
 time_table_insert = ("""
@@ -157,4 +169,4 @@ insert_table_queries = []
 # create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 # drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 # copy_table_queries = [staging_events_copy, staging_songs_copy]
-# insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+# insert_table_queries = [artist_table_insert, songplay_table_insert, user_table_insert, song_table_insert, time_table_insert]
