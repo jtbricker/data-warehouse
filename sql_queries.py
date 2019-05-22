1d3c765b47be5028962ab7fd863b8a228fe25e61
Justin T. Bricker <jtbricker@infiniteenergy.com>
2019-05-21 22:30:09 -0400

    Add SQL insert queries and allow varchar  lengths

--- sql_queries.py
+++ sql_queries.py
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS users (
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
     song_id VARCHAR PRIMARY KEY, 
-    title VARCHAR NOT NULL, 
+    title VARCHAR(2560) NOT NULL, 
     artist_id VARCHAR NOT NULL REFERENCES artists(artist_id), 
     year INT, 
     duration DECIMAL NOT NULL
@@ -92,8 +92,8 @@ CREATE TABLE IF NOT EXISTS songs (
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
     artist_id VARCHAR PRIMARY KEY, 
-    name VARCHAR NOT NULL, 
-    location VARCHAR, 
+    name VARCHAR(2560) NOT NULL, 
+    location VARCHAR(2560), 
     latitude DECIMAL, 
     longitude DECIMAL
 );
@@ -132,41 +132,68 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
+    INSERT INTO songplays ( start_time, user_id, level, song_id, artist_id, session_id, location, user_agent )
+    (
+        SELECT DISTINCT
+            timestamp 'epoch' + CAST(ste.ts AS BIGINT)/1000 * interval '1 second' as start_time,
+            ste.userid as user_id,
+            ste.level as level,
+            sts.song_id as song_id,
+            sts.artist_id as artist_id,
+            ste.sessionId as session_id,
+            ste.location as location,
+            ste.userAgent as user_agent
+        FROM staging_events ste
+        JOIN staging_songs sts
+            ON TRIM(ste.artist) = TRIM(sts.artist_name) and TRIM(ste.song) = TRIM(sts.title)
+        WHERE ste.page = 'NextSong'
+    )
 """)
 
 user_table_insert = ("""
     INSERT INTO users (
-        SELECT userId, firstName, lastName, gender, level
+        SELECT DISTINCT userId, firstName, lastName, gender, level
         FROM staging_events
-    )
+        WHERE page = 'NextSong'
+    );
 """)
 
 song_table_insert = ("""
     INSERT INTO songs (
-        SELECT song_id, title, artist_id, year, duration
+        SELECT DISTINCT song_id, title, artist_id, year, duration
         FROM staging_songs
-    )
+    );
+
 """)
 
 artist_table_insert = ("""
     INSERT INTO artists (
-	    SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude 
+	    SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude 
         FROM staging_songs
-    )
+    );
 """)
 
 time_table_insert = ("""
+    INSERT INTO time (
+        SELECT DISTINCT timestamp as start_time, 
+        DATEPART(HOUR, timestamp) as hour,
+        DATEPART(DAY, timestamp) as day,
+        DATEPART(WEEK, timestamp) as week,
+        DATEPART(MONTH, timestamp) as month,
+        DATEPART(YEAR, timestamp) as year,
+        DATEPART(DOW, timestamp) as weekday
+        FROM 
+        (
+            SELECT timestamp 'epoch' + CAST(ts AS BIGINT)/1000 * interval '1 second' as timestamp
+            FROM staging_events
+            WHERE page = 'NextSong'
+        )
+    );
 """)
 
 # QUERY LISTS
 
-create_table_queries = [staging_songs_table_create]
-drop_table_queries = [staging_songs_table_drop]
-copy_table_queries = [staging_songs_copy]
-insert_table_queries = []
-
-
-# create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
-# drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-# copy_table_queries = [staging_events_copy, staging_songs_copy]
-# insert_table_queries = [artist_table_insert, songplay_table_insert, user_table_insert, song_table_insert, time_table_insert]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [artist_table_insert, songplay_table_insert, user_table_insert, song_table_insert, time_table_insert]
